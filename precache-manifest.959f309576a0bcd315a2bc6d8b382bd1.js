916c058d6d92786e04fa6bb7f46850cf0e8841b0
root <root@bogdan.kulikov2005.fvds.ru>
2020-03-19 00:02:09 +0300

    Updates

---
+++ precache-manifest.959f309576a0bcd315a2bc6d8b382bd1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b64a2d828e5cffb9c9a363e2f40812",
+    "url": "/service/index.html"
+  },
+  {
+    "revision": "c8c8fa387e7b1283f873",
+    "url": "/service/static/css/2.128be9fc.chunk.css"
+  },
+  {
+    "revision": "4c9c2c90bf01de23549b",
+    "url": "/service/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "c8c8fa387e7b1283f873",
+    "url": "/service/static/js/2.8af620ab.chunk.js"
+  },
+  {
+    "revision": "5a768c182a70240d1505314e62be011b",
+    "url": "/service/static/js/2.8af620ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c9c2c90bf01de23549b",
+    "url": "/service/static/js/main.ff00dbfd.chunk.js"
+  },
+  {
+    "revision": "f3ca990ad53ebfac713c",
+    "url": "/service/static/js/runtime-main.69cf5501.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/service/static/media/persik.4e1ec840.png"
+  }
+]);
